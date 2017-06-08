bef65368bd3c8575adb401c91e337954e396cbe5
William Estoque <william.estoque@gmail.com>
2017-06-09 02:45:30 +0800

    Fix broken dependencies

--- use_chrome_stable_version.sh
+++ use_chrome_stable_version.sh
@@ -2,7 +2,7 @@
 
 function use_chrome_stable_version {
   curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-  sudo dpkg -i google-chrome.deb;
+  sudo dpkg -i google-chrome.deb; sudo apt-get -f install
   sudo sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
   rm google-chrome.deb
 }
